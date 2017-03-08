3982ce84cce9b385e1e8885d61d99585ccc4dbf3
eduardogit <itenriquez.isc@gmail.com>
2017-03-08 00:40:17 -0600

    First commit, pushing front view and post function

---
+++ form.js
@@ -0,0 +1,32 @@
+
+   chrome.tabs.getSelected(null, function(tab) {
+       var tabId = tab.id;
+       tabUrl = tab.url;
+
+       //con la linea de abajo se oculta el inpu 
+       document.getElementById('title').value = tabUrl;
+       //document.write(tabUrl);
+       //sendCurrentUrl(tabUrl)
+   });
+$('#send').on('click',function(){
+  console.log("etc")
+})
+function sendCurrentUrl(url) {
+  var req = new XMLHttpRequest();
+  req.addEventListener('readystatechange', function (evt) {
+    if (req.readyState === 4) {
+      if (req.status === 200) {
+        alert('Saved !');
+      } else {
+        alert("ERROR: status " + req.status);
+      }
+    }
+  });
+  req.open('POST', 'http://localhost:80/extension/insert.php', true);
+  req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  req.send('url=' + encodeURIComponent(url));
+}
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+   sendCurrentUrl(tab.url);
+});
